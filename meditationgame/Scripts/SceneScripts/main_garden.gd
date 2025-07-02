e4bf025635ff451971318135c7c1dbc235dc0c61
LeekyOnion <97726326+LeekyOnion@users.noreply.github.com>
2025-07-02 13:05:37 -0500

    Blocking for camera created - blocking new sand appearance - refactoring

--- meditationgame/Scripts/SceneScripts/main_garden.gd
+++ meditationgame/Scripts/SceneScripts/main_garden.gd
@@ -4,7 +4,7 @@ class_name Main_Garden
 @export var hud           : CanvasLayer
 @export var journal       : Control
 @export var inventory_hud : CanvasLayer
-@export var tile          : GridMap
+#@export var tile          : GridMap
 
 func _ready() -> void:
 	pass
